95234114dbed43a866defeecf1c615fd5981bfc9
Montoya Montes <pedro.montoya@cvjobs.com.mx>
2022-12-01 17:00:46 -0600

    Add map and router to that dashboard also styledcomponents

--- pages/tableros/index.js
+++ pages/tableros/index.js
@@ -1,21 +1,91 @@
 import { Button } from "@material-ui/core";
 import { Router } from "next/router";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 export default function Tablero() {
   const router = useRouter();
+  const [boards, setBoards] = useState([]);
+
+  useEffect(() => {
+    setBoards(["1", "dos", "otro"]);
+  }, []);
 
   const handleClickBoard = (board) => {
     router.push({
-      pathname: `/tableros/${123}`,
+      pathname: `/tableros/${board}`,
     });
   };
+
   return (
-    <div>
+    <DashboardStyle>
       {/* Aqui se hace un map de los tablero dentro de un grid de material ui responsivo */}
-      <Button variant="outlined" onClick={() => handleClickBoard("123")}>
-        Click para ir a tablero 123
-      </Button>
-    </div>
+      <Button variant="outlined">Click para ir a tablero 123</Button>
+      {boards.map((currentBoard, index) => (
+        <div
+          key={index}
+          class="letter"
+          onClick={() => handleClickBoard(currentBoard)}
+        >
+          Nombre del tablero:{currentBoard}
+        </div>
+      ))}
+    </DashboardStyle>
   );
 }
+
+const DashboardStyle = styled.div`
+  .letter {
+    background: #fafafa;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3),
+      0 0 300px 25px rgba(222, 198, 122, 0.7) inset;
+    width: 250px;
+    height: 350px;
+    margin: 1.5% 50% auto;
+    left: -225px;
+    padding: 24px;
+    position: relative;
+  }
+
+  .letter:before,
+  .letter:after {
+    content: "";
+    background: #fafafa;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.2),
+      inset 0 0 300px rgba(222, 198, 122, 0.7);
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    z-index: -2;
+    transition: 0.5s;
+  }
+
+  .letter:before {
+    left: -5px;
+    top: 2px;
+    transform: rotate(-1.5deg);
+  }
+
+  .letter:after {
+    right: -3px;
+    top: 0px;
+    transform: rotate(2.4deg);
+  }
+
+  .letter:hover:before {
+    transform: rotate(0deg);
+    border: solid rgba(111, 99, 61, 0.4);
+    border-width: 0px 0px 0px 1px;
+    left: -6px;
+    top: -6px;
+  }
+
+  .letter:hover:after {
+    transform: rotate(0deg);
+    border: solid rgba(111, 99, 61, 0.4);
+    border-width: 0px 0px 0px 1px;
+    right: 3px;
+    top: -3px;
+  }
+`;
